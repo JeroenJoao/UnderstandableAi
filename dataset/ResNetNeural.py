cdf6d230343987e4310b01e837f239268c8cd32a
JeroenJoao <47522472+JeroenJoao@users.noreply.github.com>
2020-02-26 13:17:49 +0100

    changes

--- dataset/ResNetNeural.py
+++ dataset/ResNetNeural.py
@@ -63,4 +63,5 @@ def getLayerPlot(img, layer):
         plt.grid(False)
         plt.imshow(display_grid, aspect='auto', cmap='viridis')
     plt.savefig(os.path.join(BASE_DIR, 'dataset/ResNetSet/image.png'))
+    plt.close(1)
     K.clear_session()
