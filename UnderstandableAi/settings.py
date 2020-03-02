7d9c5a43af216f9d5b5ae661fd69dde65ec38ac1
JeroenJoao <47522472+JeroenJoao@users.noreply.github.com>
2020-03-02 12:08:51 +0100

    cors headers update

--- UnderstandableAi/settings.py
+++ UnderstandableAi/settings.py
@@ -41,17 +41,40 @@ INSTALLED_APPS = [
     "rest_framework_api_key",
     'dataset',
     'upload',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = [
+    "https://localhost:8080",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "https://127.0.0.1:8080",
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'x-api-key',
+]
+
 
 ROOT_URLCONF = 'UnderstandableAi.urls'
 
